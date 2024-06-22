932d488b0ffebaebeb64730c49cd01f70fb5cba8
Riya Gupta <riyaaaaaaaa8@gmail.com>
2024-06-22 20:33:32 +0530

    first page

--- src/App.js
+++ src/App.js
@@ -4,20 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src="/infa-logo.svg" alt="" />
       </header>
+      <div>
+        <p>Username</p>
+        <input value="" placeholder='Your Name' />
+        <p>Password</p>
+        <input value="" placeholder='Your Password' />
+        <p>Login</p>
+        <button>Login</button>
+      </div>
     </div>
   );
 }
