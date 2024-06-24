fea154bb8267ce53dbbd077df5f30d30eb8e4e12
Riya Gupta <riyaaaaaaaa8@gmail.com>
2024-06-24 16:54:20 +0530

    second page

--- src/App.js
+++ src/App.js
@@ -1,55 +1,50 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  function applyEbf() {
-    const fileInput = document.getElementById('ebf-upload');
-    if (fileInput.files.length > 0) {
-        alert('EBF applied: ' + fileInput.files[0].name);
-    } else {
-        alert('Please select an EBF file to upload.');
-    }
-}
 
-function createOrs() {
-    alert('Create ORS functionality coming soon!');
-}
-
-function importOrs() {
-    alert('Import ORS functionality coming soon!');
-}
+  const [sidebar,setSidebar]=useState(true);
 
   return (
-    <div className="App">
-      <div class="navbar">
-        <img src="/infa-logo.png" alt="Informatica Logo"/>
-        <h1>EnvBot</h1>
-    </div>
-    <div class="container">
-        <div class="window">
-            <h2>Apply EBF</h2>
-            <input type="file" id="ebf-upload"/>
-            <button onclick="applyEbf()">Upload and Apply</button>
+    <div className='home-container'>
+      <div className='home-navbar'>
+        <div className='nav-left'>
+          <img onClick={()=>setSidebar(!sidebar)} src="menu.png" className="menu-icon" alt="" />
+          <img src="logo.png" alt="" className='infa-logo-icon' />
         </div>
-        <div class="window">
-            <h2>Create and Import ORS</h2>
-            <button onclick="createOrs()">Create ORS</button>
-            <button onclick="importOrs()">Import ORS</button>
+        <div className='nav-right'>
+          <p className='nav-right-text'>admin</p>
+          <img src="help.png" alt="" />
         </div>
-    </div>
+      </div>
+      {sidebar&&
+        <div className='sidebar-container'>
+          <div className='sidebar-box'>
+            <img src="home.jpg" alt=""/>
+            <p className='sidebar-text'>Home</p>
+          </div>
+          <div className='sidebar-box'>
+            <img src="home.jpg" alt="" />
+            <p className='sidebar-text'>Apply EBF</p>
+          </div>
+          <div className='sidebar-box'>
+            <img src="home.jpg" alt="" />
+            <p className='sidebar-text'>Create ORS</p>
+          </div>
+          <div className='sidebar-box'>
+            <img src="home.jpg" alt="" />
+            <p className='sidebar-text'>Import ORS</p>
+          </div>
+          <div className='sidebar-box'>
+            <img src="home.jpg" alt="" />
+            <p className='sidebar-text'>Restart</p>
+          </div>
+        </div>
+      }
+
     </div>
   );
 }
 
 export default App;
 
-{/* <header>
-        <img src="/infa-logo.svg" alt="" />
-      </header>
-      <div className='form'>
-        <p>Username</p>
-        <input value="" placeholder='Your Name' />
-        <p>Password</p>
-        <input value="" placeholder='Your Password' />
-        <p>Login</p>
-        <button>Login</button>
-      </div> */}
